47f3210c6de5db1b7815ade893dcd505fbe503e5
ihor.hula <ihor.hula@fiverr.com>
2025-04-09 06:37:45 +0300

    Fix nested Router issue in App.tsx

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { AppProvider, useApp } from './context/AppContext';
 import { theme } from './theme';
@@ -77,9 +77,7 @@ const App: React.FC = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppProvider>
-        <BrowserRouter>
-          <AppContent />
-        </BrowserRouter>
+        <AppContent />
       </AppProvider>
     </ThemeProvider>
   );
